471688826a281b52e30088861cdedee3cd9d6dbd
SilenceGrowth <1261900708@qq.com>
2021-03-26 16:43:58 +0800

    变一个名字

--- HwUgSdkAar/src/main/java/com/hwug/sdkaar/Test.java
+++ HwUgSdkAar/src/main/java/com/hwug/sdkaar/Test.java
@@ -6,4 +6,8 @@ public class Test {
     public static void print(){
         Log.e("data","-------Test--print ---");
     }
+
+    public static void print2(){
+        Log.e("data","-------Test--print2 ---");
+    }
 }
