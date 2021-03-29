a22315f47b641857adbca7602551b6c3566c19a1
SilenceGrowth <1261900708@qq.com>
2021-03-29 17:55:52 +0800

    api 引用 jar

--- HwUgSdkAar/src/main/java/com/hwug/a_sdkaar/HwUgBleSdkEntrance.java
+++ HwUgSdkAar/src/main/java/com/hwug/a_sdkaar/HwUgBleSdkEntrance.java
@@ -11,9 +11,10 @@ import com.ugee.pentabletinterfacelibrary.ITabletDataCallback;
 @Keep
 public final class HwUgBleSdkEntrance {
 
-    public void connect (Context context, BleDevice bleDevice,ITabletDataCallback iTabletDataCallback){
+    public void connect(Context context, BleDevice bleDevice,ITabletDataCallback iTabletDataCallback){
         UgBleFactory.getInstance().connect(context,bleDevice,iTabletDataCallback);
     }
+
     //
     public void disconnect(){
         UgBleFactory.getInstance().disConnect();
